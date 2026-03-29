b38e755a98bba1ab08f09fc2e1516f2e059e95d0
pipidaster180 <keronbit4546@gmail.com>
2026-03-29 19:31:20 +0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Раздача файлов из папки public
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+    console.log('Новое анонимное подключение...');
+
+    socket.on('chat message', (data) => {
+        // Добавляем время к сообщению
+        const msgData = {
+            nick: data.nick || "ANON",
+            text: data.text,
+            time: new Date().toLocaleTimeString('ru-RU', { hour: '2-digit', minute: '2-digit' })
+        };
+        io.emit('chat message', msgData);
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(Сервер запущен на порту ${PORT});
+});
