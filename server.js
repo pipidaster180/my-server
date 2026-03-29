768ba98bce6c6b4b3836a5ad5f3e81e57f67fecd
pipidaster180 <keronbit4546@gmail.com>
2026-03-29 19:44:16 +0500

    Update server.js

--- server.js
+++ server.js
@@ -26,5 +26,5 @@ io.on('connection', (socket) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(Сервер запущен на порту ${PORT});
+    console.log(Server started on port ${PORT});
 });
